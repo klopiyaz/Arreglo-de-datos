4a805f29fed08588fe9c9986705e52f60e4076f3
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-08-18 16:04:41 -0500

    Update modificacion nombres y estaturas.cs

--- modificacion nombres y estaturas.cs
+++ modificacion nombres y estaturas.cs
@@ -13,7 +13,7 @@ namespace ConsoleApp65
             string[] nombres = { "tanjiro", "nezuko", "zenitsu", "inosuke", "genya", "kanao", "tomioka" };
             double[] estatura = { 165, 153, 164.5, 164, 180, 156, 176 };
 
-            for (int j = 0; j > estatura.Length; j++)
+            for (int j = 0; j < estatura.Length - 1; j++)
             {
                 for (int i = 0; i < estatura.Length - 1; i++)
                 {
@@ -25,10 +25,10 @@ namespace ConsoleApp65
                         string temp = nombres[i];
                         nombres[i] = nombres[i + 1];
                         nombres[i + 1] = temp;
-                        Console.WriteLine(nombres[i] + " con estatura  de = " + estatura[i]);
                     }
                 }
             }
+            Console.WriteLine(nombres + " con estatura  de = " + estatura);
         }
     }
 }
