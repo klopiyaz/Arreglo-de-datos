73a504a6cd7e0b30722057c65d59d3c757a36dc8
klopiyaz <68033396+klopiyaz@users.noreply.github.com>
2020-08-20 11:24:06 -0500

    Add files via upload

--- blackjack con arreglos.cs
+++ blackjack con arreglos.cs
@@ -4,14 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp67
+namespace ConsoleApp68
 {
     class Program
     {
         static void Main(string[] args)
         {
             Random aleatorio = new Random();
-            int carta = 0, carta2 = 0, cartaAd = 0, total = 0, contador = 1, max = 0, sumacartas = 0, jugadoresactivos = 0; 
+            int carta = 0, carta2 = 0, cartaAd = 0, total = 0, contador = 1, max = 0, sumacartas = 0, jugadoresactivos = 0;
             string otracarta = "s";
 
             Console.Write("Ingrese el numero de jugadores = ");
@@ -29,70 +29,59 @@ namespace ConsoleApp67
 
             for (int i = 0; i < nombres.Length; i++)
             {
-                Console.WriteLine("Como te llamas = " + nombres);
+                Console.WriteLine("Como te llamas = ");
                 nombres[i] = Console.ReadLine();
             }
 
             for (int i = 0; i < nombres.Length; i++)
-            {           
-                while (jugadoresactivos != 0)
+            { // Esta condición es redundante, porque ya tengo el ciclo FOR para cada jugador
+                Console.WriteLine("\nBienvenido jugador: " + nombres[i]);
+                carta = aleatorio.Next(1, 11);
+                carta2 = aleatorio.Next(1, 11);
+                Console.Write("\nCarta1 = " + carta);
+                Console.Write("   Carta2 = " + carta2);
+                total = carta + carta2;
+                puntaje[i] = total;// Este es uno de los errores, el primer total no se almacenaba así que si uno no pedía cartas adicionales NUNCA guardaba el puntaje
+
+                Console.Write("\nllevas = " + total);
+                while (sumacartas < 21)
                 {
-                    Console.WriteLine("\nBienvenido jugador: " + contador);
-                    carta = aleatorio.Next(1, 11);
-                    carta2 = aleatorio.Next(1, 11);
-                    Console.Write("\nCarta1 = " + carta);
-                    Console.Write("   Carta2 = " + carta2);
-                    total = carta + carta2;
-                    Console.Write("\nllevas = " + total);
-                    while (sumacartas < 21)
+                    Console.WriteLine(" \nOtra carta? (s/n): ");
+                    otracarta = Console.ReadLine();
+                    while (otracarta != "s" && otracarta != "n")
                     {
+                        Console.WriteLine("Error, no se acepta otra respuesta");
                         Console.WriteLine(" \nOtra carta? (s/n): ");
                         otracarta = Console.ReadLine();
-                        while (otracarta != "s" && otracarta != "n")
+                    }
+                    if (otracarta == "s")
+                    {
+                        cartaAd = aleatorio.Next(1, 11);
+                        Console.WriteLine("\nCarta Adicional = " + cartaAd);
+                        total = cartaAd + total;
+                        Console.WriteLine("llevas = " + total);
+                        puntaje[i] = total;
+                        if (total > 21)
                         {
-                            Console.WriteLine("Error, no se acepta otra respuesta");
-                            Console.WriteLine(" \nOtra carta? (s/n): ");
-                            otracarta = Console.ReadLine();
-                        }
-                        if (otracarta == "s")
-                        {
-                            cartaAd = aleatorio.Next(1, 11);
-                            Console.WriteLine("\nCarta Adicional = " + cartaAd);
-                            total = cartaAd + total;
-                            Console.WriteLine("llevas = " + total);
-                            puntaje[i] = total;
-                            if (total > 21)
-                            {
-                                Console.WriteLine(" (Pierdes)");
-                                otracarta = "n";
-                                jugadoresactivos--;
-                                break;
-                            }
-                            else if (total == 21)
-                            {
-                                total = 21;
-                                Console.WriteLine(" (Ganas)");
-                                otracarta = "n";
-                                jugadoresactivos--;
-                                break;
-                            }
-                        }
-                        if (otracarta == "n")
-                        {
-                            jugadoresactivos--;
+                            Console.WriteLine(" (Pierdes)");
                             break;
-                        }                        
-                        if (total > max)
+                        }
+                        else if (total == 21)
                         {
-                            max = total;
+                            Console.WriteLine(" (Ganas)");
+                            break;
                         }
                     }
-                    contador++;
-                    if (contador > jugadores)
+                    if (otracarta == "n")
                     {
+                        Console.WriteLine("Terminaste con = " + total);
                         break;
                     }
                 }
+            }
+            for (int i = 0; i < nombres.Length; i++)
+            {
+                Console.WriteLine(nombres[i] + " = " + puntaje[i]);
             }
             for (int j = 0; j < nombres.Length - 1; j++)
             {
@@ -110,11 +99,7 @@ namespace ConsoleApp67
                     }
                 }
             }
-            for (int i = 0; i < nombres.Length; i++)
-            {
-                Console.WriteLine(nombres[i] + " = " + puntaje[i]);
-            }
-            Console.WriteLine("El ganador es = "  + nombres[0]);
+            Console.WriteLine("El ganador es = " + nombres[0]);
             Console.WriteLine("El segundo lugar es = " + nombres[2]);
             Console.WriteLine("el ganador es = " + max);
         }
